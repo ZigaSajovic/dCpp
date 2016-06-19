add1d885f0f39561c2ab252978f56fd4e5e3c667
ZigaSajovic <ziga.sajovic@gmail.com>
2016-06-19 02:49:32 +0200

    deleted test files

--- src/Matrix.cpp
+++
@@ -1,5 +0,0 @@
-#include "Matrix.h"
-
-Matrix::Matrix(int n, int m){block= new double[n*m];}
-
-Matrix::~Matrix(){delete block;}
--- src/testMap.cpp
+++
@@ -1,6 +0,0 @@
-#include "testMap.h"
-
-
-testMap::~testMap(){}
-
-testMap::testMap(func a, func b){mapping=a;primitive=b;}
