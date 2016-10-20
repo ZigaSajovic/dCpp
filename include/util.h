35158d26bc110ca67f3e6aff490d461ff7db30d8
ZigaSajovic <ziga.sajovic@gmail.com>
2016-10-21 01:48:05 +0200

    modified util.h

--- include/util.h
+++ include/util.h
@@ -14,32 +14,6 @@ template<class InputIterator,class InputIterator2, class OutputIterT, class Func
   return result;
 }
 
-template<class InputIterT1, class InputIterT2, class OutputIterT, class Comparator, class Func, class Func2, class Sc>
-OutputIterT merge_apply2(
-    InputIterT1 first1, InputIterT1 last1,
-    InputIterT2 first2, InputIterT2 last2,
-    OutputIterT result, Comparator comp, Func func, Func2 func2, Sc n) {
-  while (true)
-  {
-    if(first1==last1) return for_each_copy(first2, last2, result, func2,n);
-    if(first2==last2) return for_each_copy(first1, last1, result,func2,n);
-
-    if (comp(*first1, *first2)<0) {
-      *result = *first1;
-      ++first1;
-    } else if (comp(*first1, *first2)>0) {
-      *result=*first2;
-      ++first2;
-    } else {
-      *result=func(*first1, *first2);
-      ++first1;
-      ++first2;
-    }
-    ++result;
-  }
-}
-
-
 template<class InputIterT1, class InputIterT2, class OutputIterT, class Comparator, class Func>
 OutputIterT merge_apply(
     InputIterT1 first1, InputIterT1 last1,
