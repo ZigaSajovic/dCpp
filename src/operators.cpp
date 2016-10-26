ccdeba5b1213af88d2ca543b04e68ed2dd034c5d
ZigaSajovic <ziga.sajovic@gmail.com>
2016-10-26 02:49:31 +0200

    inplace optimization

--- src/operators.cpp
+++ src/operators.cpp
@@ -22,7 +22,7 @@ var operator^(double n, const var& v){
     out.id=std::pow(n,v.id);
     out.order=v.order;
     if(v.order>0){
-        for_each_copy(v.dTau.get()->begin(),v.dTau.get()->end(),inserter(*(out.dTau.get()),out.dTau.get()->begin()),mul_make_pair<std::pair<var*,var> ,var >, std::log(n)*(n^(v.reduce())));
+        for_each_copy(v.dTau.get()->begin(),v.dTau.get()->end(),inserter(*(out.dTau.get()),out.dTau.get()->begin()),mul_make_pair<std::pair<var*,var> ,var >, (n^(v.reduce()))*=std::log(n));
     }
     return out;
 }
