3abc892f4d0fc3a10e8e1f953281fe31bdb4ca1b
ZigaSajovic <ziga.sajovic@gmail.com>
2017-01-29 05:14:53 +0100

    Minor correction in tau.cpp

--- src/tau.cpp
+++ src/tau.cpp
@@ -14,8 +14,10 @@ namespace dCpp{
         var out;
         out.id=this->func(v.id);
         out.order=v.order;
-        for_each_copy(v.dTau.get()->begin(),v.dTau.get()->end(),inserter(*(out.dTau.get()),out.dTau.get()->begin()),
-            mul_make_pair<std::pair<var*,var> , var >, this->primitive(v.reduce()));
+        if(v.order>0){
+            for_each_copy(v.dTau.get()->begin(),v.dTau.get()->end(),inserter(*(out.dTau.get()),out.dTau.get()->begin()),
+                mul_make_pair<std::pair<var*,var> , var >, this->primitive(v.reduce()));
+        }
         return out;
     }
 }
